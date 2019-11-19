95566ad490bb72400f30c6758b048989cb7a3c4b
Nij4life <nij4life@gmail.com>
2019-11-19 18:13:19 +0300

    refactor: ref and fix

--- assets/components/keyboard/keyboard.js
+++ assets/components/keyboard/keyboard.js
@@ -1,5 +1,6 @@
 import languages from '../../abstracts/languages.js';
 import signs from '../../abstracts/signs.js';
+
 class Keyboard {
     constructor(textarea) {
         this.textarea = textarea;
@@ -7,7 +8,7 @@ class Keyboard {
         this._useStorage();
         this.capsLock = false;
         this.keyboard = this.initialize();
-        this.listeners();
+        this.addListeners();
 
         return this.keyboard;
     }
@@ -19,10 +20,9 @@ class Keyboard {
         let row = document.createElement('div');
         row.classList.add('keyboard--row');
         wrap.append(row);
-        let i = 0;
 
-        for(let cell of arr) {
-            if (i === 14 || i === 28 || i === 41 ||  i === 54) {
+        arr.forEach((cell, i) => {
+            if (i === 14 || i === 28 || i === 41 || i === 54) {
                 row = document.createElement('div');
                 row.classList.add('keyboard--row');
                 wrap.append(row);
@@ -32,8 +32,7 @@ class Keyboard {
             span.setAttribute('data-Keycode', `${cell[0]}`);
             span.textContent = `${cell[1]}`;
             row.append(span);
-            i++;
-        }
+        })
         return wrap;
     }
 
@@ -50,7 +49,7 @@ class Keyboard {
             });
     }
 
-    _setActiveClass({target}) {
+    _setActiveClass({ target }) {
         if (target.classList.contains('keyboard--item')) {
             const keycode = target.dataset.keycode;
             if (/^CapsLock$/.test(keycode) && this.capsLock) {
@@ -61,7 +60,7 @@ class Keyboard {
         }
     }
 
-    _removeActiveClass({target}) {
+    _removeActiveClass({ target }) {
         if (target.classList.contains('keyboard--item')) {
             const keycode = target.dataset.keycode;
             if (!/^CapsLock$/.test(keycode)) {
@@ -78,16 +77,16 @@ class Keyboard {
 
     _lettersUp() {
         const elems = this.keyboard.querySelectorAll('.keyboard--item');
-        for (let x of elems) {
-            if (/^Key/.test(x.dataset.keycode)) x.textContent = x.textContent.toUpperCase();
-        }
+        elems.forEach(el => {
+            if (/^Key/.test(el.dataset.keycode)) el.textContent = el.textContent.toUpperCase();
+        });
     }
 
     _lettersDown() {
         const elems = this.keyboard.querySelectorAll('.keyboard--item');
-        for (let x of elems) {
-            if (/^Key/.test(x.dataset.keycode)) x.textContent = x.textContent.toLowerCase();
-        }
+        elems.forEach(el => {
+            if (/^Key/.test(el.dataset.keycode)) el.textContent = el.textContent.toLowerCase();
+        });
     }
 
     _useShift() {
@@ -96,55 +95,66 @@ class Keyboard {
             this.keyboard.querySelector(`[data-keycode="${el[0]}"]`).textContent = el[1];
         });
 
-        this._lettersUp();
+        !this.capsLock ? this._lettersUp() : this._lettersDown();
     }
 
-    _listenerLeave({target, code}) {
-        if (code !== 'ShiftLeft' || code !== 'ShiftRight') {
-            target.addEventListener('mouseleave', e => e.target.classList.remove('active'), {once: true});
-        }
+    _listenerLeave({ target }) {
+        target.addEventListener('mouseleave', e => {
+            e.target.classList.remove('active');
+            if (target.dataset.keycode === 'ShiftLeft' || target.dataset.keycode === 'ShiftRight') {
+                this._lettersDown();
+            }
+        }, { once: true });
     }
 
-    listeners() {
+    addListeners() {
         window.addEventListener('DOMContentLoaded', () => {
-            this.listenReal();
-            this.listenVirtual();
+            this.addListenerRealKeyboard();
+            this.addListenVirtualKeyboard();
         });
     }
 
-    listenReal() {
+    addListenerRealKeyboard() {
         document.addEventListener('keydown', (e) => {
-            if (e.code === 'CapsLock') {
-                this.capsLock = (this.capsLock === true) ? false : true;
-                this.keyboard.querySelector(`[data-keycode=${e.code}]`).classList.toggle('active');
-                if (this.capsLock) {
-                    this._lettersUp();
-                } else {
-                    this._lettersDown();
-                }
+            switch (e.code) {
+                case 'CapsLock':
+                    this.capsLock = !this.capsLock;
+                    this.keyboard.querySelector(`[data-keycode=${e.code}]`).classList.toggle('active');
+                    this.capsLock ? this._lettersUp() : this._lettersDown();
+                    break;
 
-            } else if (this.keyboard.querySelector(`[data-keycode=${e.code}]`)) {
-                this.keyboard.querySelector(`[data-keycode=${e.code}]`).classList.add('active');
-            }
-
-            if (e.code === 'Tab') {
-                let indexСarriage = this.textarea.selectionStart;
-                let text = this.textarea.value;
-                this.textarea.value = [...text.slice(0, indexСarriage), '    ', ...text.slice(indexСarriage)].join('');
-                this.textarea.selectionEnd = indexСarriage + 4;
-                e.preventDefault();
-            }
-
-            if (e.code === 'AltRight' || e.code === 'AltLeft') {
-                if (e.shiftKey) {
-                    this.changeLang();
-                } else {
+                case 'Tab': {
+                    let indexСarriage = this.textarea.selectionStart;
+                    let text = this.textarea.value;
+                    this.textarea.value = [...text.slice(0, indexСarriage), '    ', ...text.slice(indexСarriage)].join('');
+                    this.textarea.selectionEnd = indexСarriage + 4;
                     e.preventDefault();
                 }
+                    break;
+
+                case 'AltRight':
+                case 'AltLeft':
+                    if (e.shiftKey) {
+                        this.changeLang();
+                    } else {
+                        e.preventDefault();
+                    }
+                    break;
+
+                case 'ShiftLeft':
+                        this.keyboard.querySelector(`[data-keycode='ShiftRight']`).classList.remove('active');
+                        this.keyboard.querySelector(`[data-keycode='ShiftLeft']`).classList.add('active');
+                        this._useShift();
+                        break;
+                case 'ShiftRight':
+                        this.keyboard.querySelector(`[data-keycode='ShiftLeft']`).classList.remove('active');
+                        this.keyboard.querySelector(`[data-keycode='ShiftRight']`).classList.add('active');
+                        this._useShift();
+                    break;
             }
 
-            if (e.code === 'ShiftLeft' || e.code === 'ShiftRight') {
-                this._useShift();
+            if (this.keyboard.querySelector(`[data-keycode=${e.code}]`) && e.code !== 'CapsLock') {
+                this.keyboard.querySelector(`[data-keycode=${e.code}]`).classList.add('active');
             }
         });
 
@@ -153,18 +163,24 @@ class Keyboard {
 
             if (this.keyboard.querySelector(`[data-keycode=${e.code}]`)) {
                 this.keyboard.querySelector(`[data-keycode=${e.code}]`).classList.remove('active');
-            }
 
-            if (e.code === 'ShiftLeft' || e.code === 'ShiftRight') {
-                this.fill();
+                if (e.code === 'ShiftLeft' || e.code === 'ShiftRight') {
+                    this.fill();
+
+                    if (this.capsLock) {
+                        this._lettersUp();
+                    }
+
+                    this.keyboard.querySelector(`[data-keycode=${e.code}]`).classList.remove('active');
+                }
             }
         });
     }
 
-    listenVirtual() {
+    addListenVirtualKeyboard() {
         this.keyboard.addEventListener('mousedown', (e) => {
-            let key = e.target.textContent;
             const keycode = e.target.dataset.keycode;
+            let key = e.target.textContent;
             let indexСarriage = this.textarea.selectionStart;
             let text = this.textarea.value;
 
@@ -182,34 +198,43 @@ class Keyboard {
                         this.textarea.selectionEnd = ++indexСarriage;
                     }
                 }
+
                 // speсial keys
-                if (/^Shift/.test(keycode)) {
-                    this._useShift();
-                } else if (/^Tab$/.test(keycode)) {
-                    this.textarea.value = [...text.slice(0, indexСarriage), '    ', ...text.slice(indexСarriage)].join('');
-                    this.textarea.selectionEnd = indexСarriage + 4;
-                } else if (/^Space$/.test(keycode)) {
-                    this.textarea.value = [...text.slice(0, indexСarriage-1), ' ', ...text.slice(indexСarriage-1)].join('');
-                    this.textarea.selectionEnd = indexСarriage;
-                } else if (/^Enter$/.test(keycode)) {
-                    this.textarea.value = [...text.slice(0, indexСarriage), '\n', ...text.slice(indexСarriage)].join('');
-                    this.textarea.selectionEnd = ++indexСarriage;
-                } else if (/^Backspace$/.test(keycode)) {
-                    this.textarea.value = [...text.slice(0, indexСarriage-1), ...text.slice(indexСarriage)].join('');
-                    this.textarea.selectionEnd = --indexСarriage;
-                } else if (/^CapsLock$/.test(keycode)) {
-                    this.capsLock = this.capsLock ? false : true;
-                    if (this.capsLock) {
-                        this._lettersUp();
-                    } else {
-                        this._lettersDown();
-                    }
-                } else if (/^ArrowLeft$/.test(keycode)) {
-                    this.textarea.selectionStart -= 1;
-                    this.textarea.selectionEnd -= 1;
-                } else if (/^ArrowRight$/.test(keycode)) {
-                    this.textarea.selectionEnd += 1;
-                    this.textarea.selectionStart = this.textarea.selectionEnd
+                switch (true) {
+                    case (/^Shift/.test(keycode)):
+                        this._useShift();
+                        break;
+                    case (/^Tab$/.test(keycode)):
+                        this.textarea.value = [...text.slice(0, indexСarriage), '    ', ...text.slice(indexСarriage)].join('');
+                        this.textarea.selectionEnd = indexСarriage + 4;
+                        break;
+
+                    case (/^Space$/.test(keycode)):
+                        this.textarea.value = [...text.slice(0, indexСarriage - 1), ' ', ...text.slice(indexСarriage - 1)].join('');
+                        this.textarea.selectionEnd = indexСarriage;
+                        break;
+                    case (/^Enter$/.test(keycode)):
+                        this.textarea.value = [...text.slice(0, indexСarriage), '\n', ...text.slice(indexСarriage)].join('');
+                        this.textarea.selectionEnd = ++indexСarriage;
+                        break;
+                    case (/^Backspace$/.test(keycode)):
+                        this.textarea.value = [...text.slice(0, indexСarriage - 1), ...text.slice(indexСarriage)].join('');
+                        this.textarea.selectionEnd = --indexСarriage;
+                        break;
+                    case (/^CapsLock$/.test(keycode)):
+                        this.capsLock = !this.capsLock;
+                        this.capsLock ? this._lettersUp() : this._lettersDown();
+                        break;
+                    case (/^ArrowLeft$/.test(keycode)):
+                        this.textarea.selectionStart -= 1;
+                        this.textarea.selectionEnd -= 1;
+                        break;
+                    case (/^ArrowRight$/.test(keycode)):
+                        this.textarea.selectionEnd += 1;
+                        this.textarea.selectionStart = this.textarea.selectionEnd;
+                        break;
+                    default:
+                        break;
                 }
             }
         });
